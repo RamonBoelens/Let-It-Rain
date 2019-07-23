6d7047a60c15b1f9176954e1f6c8bad2bd480dd4
RamonBoelens <ramonboelens@hotmail.com>
2019-07-23 23:10:18 +0200

    Update
    Added different enemies
    Added different enemy sizes
    Added Animation curve for spawning

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    private float xMin, xMax;
-    private float spawnCooldown = 0.3f;
+    // Spawnable GameObject
+    public GameObject[] enemyPf;
 
-    public GameObject enemyPf;
+    // Spawn rate
+    public AnimationCurve spawnRate;
+    private float timer;
+    private float currentSpawnRate;
+    private float spawnCooldown;
+
+    // Spawn Area
+    private float xMin, xMax;
 
     private void Start()
     {
@@ -16,32 +23,45 @@ public class Spawner : MonoBehaviour
         // Get the screen width
         xMin = cam.ViewportToWorldPoint(new Vector3(0, 0, 0)).x;
         xMax = cam.ViewportToWorldPoint(new Vector3(1, 0, 0)).x;
+
+        currentSpawnRate = spawnRate.Evaluate(timer);
+        ResetCooldown(currentSpawnRate);
     }
 
     private void Update()
     {
+        timer += Time.deltaTime;
+        currentSpawnRate = spawnRate.Evaluate(timer);
+
         if (spawnCooldown <= 0.0f)
         {
             SpawnObject();
-            ResetCooldown(0.3f);
-        }
-        
-        else
-        {
-            spawnCooldown =- Time.deltaTime;
+            ResetCooldown(currentSpawnRate);
+
+        } else {
+            spawnCooldown -= Time.deltaTime;
         }
     }
 
     private void SpawnObject()
     {
-        Vector2 SpawnPos = new Vector2(Random.Range(xMin, xMax / 2), transform.position.y);
-        var go = Instantiate(enemyPf, new Vector3(SpawnPos.x, SpawnPos.y, 0), transform.rotation);
+        // Get a random shape
+        int enemyID = Random.Range(0, enemyPf.Length);
+
+        // Randomize stats
+        float fallSpeed = Random.Range(3.0f, 5.0f);
+        float size = Random.Range(0.25f, 2.0f);
+
+        // Randomize Spawn Position
+        Vector2 SpawnPos = new Vector2(Random.Range(xMin, xMax), transform.position.y);
+
+        // Instantiate object
+        GameObject go = Instantiate(enemyPf[enemyID], new Vector3(SpawnPos.x, SpawnPos.y, 0), transform.rotation);
         go.transform.SetParent(gameObject.transform);
 
-        float fallSpeed = Random.Range(3.0f, 5.0f);
-
-
+        // Apply the stats
         go.GetComponent<EnemyController>().fallSpeed = fallSpeed;
+        go.transform.localScale = new Vector3(size, size, 0);
     }
 
     private void ResetCooldown(float time)
